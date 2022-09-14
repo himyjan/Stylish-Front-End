aae29b4008da428bed85548f00f6fc13a1636a87
himyjan <51815522+himyjan@users.noreply.github.com>
2022-09-14 15:35:34 +0800

    update redux store action

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,16 +7,57 @@ export const cartItemsSlice = createSlice({
   name: 'cartItems',
   initialState: initializer,
   reducers: {
-    ADD_TO_CART: (state: Product[], action: Action) => {
+    add: (state: Product[], action: Action) => {
+      function getStock(colorCode, size) {
+        return action.payload.product.variants.find(
+          (variant) => variant.color_code === colorCode && variant.size === size
+        ).stock;
+      }
+      const newCartItems: Product[] = [
+        ...state,
+        {
+          color: action.payload.product.colors.find(
+            (color) => color.code === action.payload.selectedColorCode
+          ),
+          id: action.payload.product.id,
+          image: action.payload.product.main_image,
+          name: action.payload.product.title,
+          price: action.payload.product.price,
+          qty: action.payload.quantity,
+          size: action.payload.selectedSize,
+          stock: getStock(
+            action.payload.selectedColorCode,
+            action.payload.selectedSize
+          ),
+        },
+      ];
+      window.alert('已加入商品');
+      window.localStorage.setItem('cartItems', JSON.stringify(newCartItems));
       state = action.payload as Product[];
     },
-    REMOVE_FROM_CART: (state: Product[], action: Action) => {
+    changeQuantity: (state: Product[], action: Action) => {
+      const newCartItems = state.map((item, index) =>
+        index === action.payload.itemIndex
+          ? {
+              ...item,
+              qty: action.payload.itemQuantity,
+            }
+          : item
+      );
+
+      window.alert('已修改商品數量');
+      window.localStorage.setItem('cartItems', JSON.stringify(newCartItems));
       state = action.payload as Product[];
     },
-    DECREMENT_QUANTITY: (state: Product[], action: Action) => {
+    delete: (state: Product[], action: Action) => {
+      const newCartItems = state.filter(
+        (_, index) => index !== action.payload.itemIndex
+      );
+      window.alert('已刪除商品');
+      window.localStorage.setItem('cartItems', JSON.stringify(newCartItems));
       state = action.payload as Product[];
     },
-    CLEAR_CART: (state: Product[], action: Action) => {
+    clear: (state: Product[], action: Action) => {
       state.length = 0;
     },
   },
