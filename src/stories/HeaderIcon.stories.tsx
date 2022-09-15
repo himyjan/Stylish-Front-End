38a7cf46e30e8ce077a18a184fb49d714c5a641e
himyjan <51815522+himyjan@users.noreply.github.com>
2022-09-15 17:00:24 +0800

    upload stylish storybook

---
+++ src/stories/HeaderIcon.stories.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import HeaderIcon from './HeaderIcon';
+
+export default {
+  title: 'STYLiSH/HeaderIcon',
+  component: HeaderIcon,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+const Template = (args) => <HeaderIcon {...args} />;
+
+export const IconWithNumber = Template.bind({});
+IconWithNumber.args = {
+  icon: 'cart',
+  cartItemsLength: 1,
+  text: '購物車',
+};
+
+export const CartIconWithoutNumber = Template.bind({});
+CartIconWithoutNumber.args = {
+  icon: 'cart',
+};
+export const ProfileIconWithoutNumber = Template.bind({});
+ProfileIconWithoutNumber.args = {
+  icon: 'user',
+};
---
+++ src/stories/Logo.stories.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Logo from './Logo';
+
+export default {
+  title: 'STYLiSH/Logo',
+  component: Logo,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+const Template = (args) => <Logo {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  logoPicture:
+    'https://cdn.discordapp.com/attachments/1015532616969105419/1019825483803799573/logo.png',
+};
