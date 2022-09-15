3b49afde87036f83513f329ab6cc7d752745fa83
himyjan <51815522+himyjan@users.noreply.github.com>
2022-09-15 09:36:45 +0800

    remove redux core

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,26 @@
-import { createStore, combineReducers } from 'redux';
 import { cartItemsReducer } from '../reducers/index';
 
-const rootReducer = combineReducers({
-  cartItemsReducer,
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const cartItemsSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    incremented: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1;
+    },
+    decremented: (state) => {
+      state.value -= 1;
+    },
+  },
 });
 
-export const store = createStore(rootReducer);
+export const store = configureStore({
+  reducer: cartItemsSlice.reducer,
+});
